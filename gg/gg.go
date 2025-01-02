2058315bc41a68f8f1d1a4861014897bc6b5af59
mancuoj <mancuoj@gmail.com>
2025-01-03 00:11:28 +0800

    feat: engine, router, context

--- gg/gg.go
+++ gg/gg.go
@@ -1,38 +1,40 @@
 package gg
 
 import (
-	"fmt"
 	"net/http"
 )
 
+type HandlerFunc func(*Context)
+
 type Engine struct {
-	router map[string]http.HandlerFunc
+	router *router
 }
 
+// 1. 先新建一个 Engine
+// 2. 然后往 Engine 的 router 里添加路由及其对应的 handler
+// 3. 启动服务，然后所有的请求都会由 engine.ServeHTTP 处理
+// 4. 每个请求都新建一个 Context 对象
+// 5. 找到请求 URL 对应的 handler
+// 6. 调用 handler 处理请求
 func New() *Engine {
-	return &Engine{router: make(map[string]http.HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler http.HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) GET(pattern string, handler http.HandlerFunc) {
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
 	engine.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler http.HandlerFunc) {
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
 	engine.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
 
 func (engine *Engine) Run(addr string) (err error) {
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
@@ -11,14 +10,19 @@ import (
 func main() {
 	r := gg.New()
 
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *gg.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *gg.Context) {
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *gg.Context) {
+		c.JSON(http.StatusOK, gg.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	log.Printf("Server is running on http://localhost:9999")
